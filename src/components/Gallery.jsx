0488e80047d6ffeb3d6788b8814f019fbd00aed7
Mesfin67 <140214673+Mesfin67@users.noreply.github.com>
2025-05-18 13:48:31 +0300

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -9,13 +9,13 @@ const Gallery = () => {
         <h3 className="text-center mb-5">Gallery</h3>
         <Row>
           <Col md={4} sm={6} xs={12} className="mb-4">
-            <Image src="plane9.webp" alt="Gallery Image 1" rounded fluid />
+            <Image src="aa.jpg" alt="Gallery Image 1" rounded fluid />
           </Col>
           <Col md={4} sm={6} xs={12} className="mb-4">
-            <Image src="plane9.webp" alt="Gallery Image 2" rounded fluid />
+            <Image src="aa.jpg" alt="Gallery Image 2" rounded fluid />
           </Col>
           <Col md={4} sm={6} xs={12} className="mb-4">
-            <Image src="plane9.webp" alt="Gallery Image 3" rounded fluid />
+            <Image src="aa.jpg" alt="Gallery Image 3" rounded fluid />
           </Col>
         </Row>
       </Container>
